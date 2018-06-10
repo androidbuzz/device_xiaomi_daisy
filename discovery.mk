69ffed2130952d08b65d6f3a969aadd58d75c6c9
tissotsaksham <briantissot72@gmail.com>
2018-06-10 17:40:02 +0000

    Add missing device information

--- discovery.mk
+++ discovery.mk
@@ -57,13 +57,15 @@ PRODUCT_NAME := discovery_tissot
 BOARD_VENDOR := Xiaomi
 PRODUCT_DEVICE := tissot
 
+TARGET_VENDOR_PRODUCT_NAME := tissot
+TARGET_VENDOR_DEVICE_NAME := tissot
+
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="tissot-user 8.0.0 OPR1.170623.026 8.1.10 release-keys"
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    DEVICE_MAINTAINERS="saksham | @zeroatlol"
+DEVICE_MAINTAINERS="saksham | @zeroatlol"
 
 BUILD_FINGERPRINT := xiaomi/tissot/tissot_sprout:8.0.0/OPR1.170623.026/8.1.10:user/release-keys
 
