5a10a73762cceeb381bacca448527e99fff1c573
tissotsaksham <briantissot72@gmail.com>
2018-06-11 10:24:28 +0000

    Revert "Update Android.mk"
    This reverts commit cbee4a039629ac02412ee9659d2c2026385cc578.

--- libhidl/Android.mk
+++ libhidl/Android.mk
@@ -18,7 +18,6 @@ LOCAL_SHARED_LIBRARIES := libhidltransport
 LOCAL_MODULE := android.hidl.base@1.0 
 LOCAL_MODULE_TAGS := optional 
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_VENDOR_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS) 
@@ -27,7 +26,6 @@ LOCAL_MODULE := android.hidl.base@1.0_system
 LOCAL_INSTALLED_MODULE_STEM := android.hidl.base@1.0.so 
 LOCAL_MODULE_TAGS := optional 
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_VENDOR_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS) 
@@ -35,5 +33,4 @@ LOCAL_SHARED_LIBRARIES := libhidltransport
 LOCAL_MODULE := android.hidl.manager@1.0 
 LOCAL_MODULE_TAGS := optional 
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_VENDOR_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
