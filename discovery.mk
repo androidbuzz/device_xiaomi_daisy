c42b180695bca5e42492d86ecf85b21bac36bbb1
Salvatore Festa <salvatorefestatech@gmail.com>
2018-06-14 10:04:42 +0200

    Use a simple maintainer string to avoid errors

--- discovery.mk
+++ discovery.mk
@@ -65,7 +65,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="tissot-user 8.0.0 OPR1.170623.026 8.1.10 release-keys"
 
-DEVICE_MAINTAINER := "saksham | @zeroatlol"
+DEVICE_MAINTAINER := Saksham
 
 BUILD_FINGERPRINT := xiaomi/tissot/tissot_sprout:8.0.0/OPR1.170623.026/8.1.10:user/release-keys
 
